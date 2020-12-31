4b1bb97f1a9fbf6fb552c814dbc20995f6605162
Nathaniel-insight <nagillgren@gmail.com>
2020-12-31 13:39:19 -0800

    lots of chagnes lol

---
+++ .ipynb_checkpoints/scrape_banzuke-checkpoint.py
@@ -0,0 +1,79 @@
+"""
+modules to scrape banzuke 
+"""
+
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
+banzuke_text_url = 'http://sumodb.sumogames.de/Banzuke_text.aspx?b={}{}'
+
+def retrieve_rows(soup, start_rank = 'Makuuchi', end_rank = 'Juryo'):
+    """
+    takes banzuke soup object and grabs rows data from makuuchi and juryo
+    returns as dataframe
+    """
+    
+    if len(soup.find_all('pre')) == 0:
+        return 'None'
+    
+    l = str(soup.find_all('pre')[0]).split('\n')
+    
+    start_idx = l.index(start_rank) + 2
+    end_idx = l.index(end_rank) -1
+    
+    to_df = l[start_idx:end_idx]
+    to_df = [y.split() for y in to_df]
+    
+    cols = ['rank', 'name', 'pob', 'stable', 'birthdate', 'height', 'weight']
+    
+    df = pd.DataFrame(to_df, columns = cols)
+    
+    return df
+
+def combine_divisions(div1, div2):
+    """
+    join data frames from two different divisions
+    """
+    
+    jf = pd.concat([div1, div2])
+    
+    return jf
+
+def scrape_year(year):
+    """
+    scrape an entire years worth of data for makuuchi and juryo banzukes
+    """
+
+    months = [str(x).zfill(2) for x in list(range(1,13,1))] # banzukes only occur every 3 months (starting in january)
+    
+    
+    urls = [banzuke_text_url.format(year, x) for x in months ]
+    
+    banzuke = []
+    
+    for r in urls:
+        res = requests.get(r)
+        soup = BeautifulSoup(res.content, features="html5lib")
+        print(r)
+        mak = retrieve_rows(soup, start_rank = 'Makuuchi', end_rank = 'Juryo')
+        if type(mak) == type(str()):
+            continue
+        jur = retrieve_rows(soup, start_rank = 'Juryo', end_rank = 'Makushita')
+        
+        print(r)
+        df = combine_divisions(mak, jur)
+        df['year'] = year
+        df['month'] = r[-2::1]
+        banzuke.append(df)
+    
+    banzuke = pd.concat(banzuke, ignore_index=True)
+    
+    return df
+
+def scrape_month(year, month):
+    """
+    get banzuke results for just one month / year
+    """
+    
+    return df
---
+++ process_banzuke.py
@@ -0,0 +1,68 @@
+"""
+modules for cleaning and processing banzukes and hoshitori
+"""
+
+import pandas as pd
+
+def numerical_rank(df_rank):
+    """
+    take rank and extract numerical rank
+    """
+    
+    df_num = df_rank.str.extract(r'(\d+)').astype('float')
+    
+    return df_num
+
+def tier_rank(df_rank):
+    """
+    return ordinal ranks for Y, O, S, K, M, J
+    """
+    
+    df_tier = df_rank.str[0]
+    mapper = {
+        'Y': 1,
+        'O': 2,
+        'S': 3,
+        'K': 4,
+        'M': 5,
+        'J': 6
+    }
+    
+    df_tier = df_tier.replace(mapper)
+    
+    return df_tier
+
+def east_west(df_rank):
+    """
+    turn east west into binary
+    """
+    
+    df_is_east = df_rank.str.contains('e')
+    
+    return df_is_east
+
+def extract_ranks(df):
+    """
+    expand rank into 3 categories
+    is east
+    numerical rank
+    tier
+    """
+    
+    df['tier_rank'] = tier_rank(df['rank'])
+    df['num_rank'] = numerical_rank(df['rank'])
+    df['is_east'] = east_west(df['rank'])
+    
+    return df
+
+def abs_rank(df, cols = ['year', 'month', 'tier_rank', 'num_rank', 'is_east']):
+    """
+    order and rank results to get absoulte rank
+    """
+    
+    cols = cols
+    rf = df.sort_values(by = cols, ascending = [True, True, True, True, False],ignore_index = True)
+    rf['next_order'] = rf.index
+    rf['next_abs_rank'] = rf.groupby(['year','month'])['next_order'].rank()
+    
+    return rf
